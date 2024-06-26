dfca5045c491d11275cd355a48ef06b4ffdcb990
matheusjean28 <matheusjean28@gmail.com>
2024-06-26 18:49:02 -0300

    create a new container at other object

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const ScaffoldExampleApp());
 
-class MidContainer extends StatelessWidget {
+class ScaffoldExampleApp extends StatelessWidget {
+  const ScaffoldExampleApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      width: 200,
-      alignment: Alignment.center,
-      
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(8)),
-        color: Colors.blue,
-      ),
-      child: const Text("teste"),
+    return const MaterialApp(
+      home: ScaffoldExample(),
     );
   }
 }
 
-class MyApp extends StatelessWidget {
+class CountDownContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Demo'),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: MidContainer(),
-      ),
-    ));
+    return Column(
+      //define start position container
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 300,
+          width: 500,
+          child: const Padding(
+            padding: EdgeInsets.all(8),
+            //change card color before
+          
+            child: Card(
+              color: Colors.lightBlueAccent,
+              child: Center(
+                child: Text("data"),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ScaffoldExample extends StatefulWidget {
+  const ScaffoldExample({super.key});
+
+  @override
+  State<ScaffoldExample> createState() => _ScaffoldExampleState();
+}
+
+class _ScaffoldExampleState extends State<ScaffoldExample> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Container(
+          alignment: Alignment.center,
+          child: CountDownContainer(),
+        ));
   }
 }
